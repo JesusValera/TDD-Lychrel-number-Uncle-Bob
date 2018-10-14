6954e956909548c6a1f37c0167ec09d013e51d80
Jesus Valera <jesus1902@outlook.com>
2018-10-14 18:03:23 +0200

    Is and is not palindrome a given number.

--- src/lychrel/LichrelTest.kt
+++ src/lychrel/LichrelTest.kt
@@ -1,17 +1,48 @@
 package lychrel
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class LichrelTest {
 
+    private val limit: Int = 1000
+
     @Test
     fun facts() {
-        isNotLychrel(1, 0, 1000)
+        convergesAtIteration(1, 0)
+        convergesAtIteration(2, 0)
+        convergesAtIteration(10, 1)
+        convergesAtIteration(11, 0)
     }
 
-    private fun isNotLychrel(n: Int, iteration: Int, limit: Int) {
+    private fun convergesAtIteration(n: Int, iteration: Int) {
         assertEquals(iteration, Lychrel().convergesAtIteration(n, limit))
     }
 
+    @Test
+    fun nonPalindromes() {
+        isNotPalindrome(10)
+        isNotPalindrome(12331)
+        isNotPalindrome(1243321)
+    }
+
+    @Test
+    fun palindromes() {
+        isPalindrome(1)
+        isPalindrome(11)
+        isPalindrome(121)
+        isPalindrome(12321)
+        isPalindrome(1234321)
+    }
+
+    private fun isNotPalindrome(n: Int) {
+        assertFalse(Lychrel().isPalindrome(n))
+    }
+
+    private fun isPalindrome(n: Int) {
+        assertTrue(Lychrel().isPalindrome(n))
+    }
 }
--- src/lychrel/Lychrel.kt
+++ src/lychrel/Lychrel.kt
@@ -1,8 +1,23 @@
 package lychrel
 
 class Lychrel {
+
     fun convergesAtIteration(n: Int, limit: Int): Int {
-        return 0
+        return if (isPalindrome(n))
+            0
+        else
+            1
+    }
+
+    fun isPalindrome(n: Int): Boolean {
+        val digits = n.toString()
+        val lastIndex = digits.length - 1
+
+        for (i in 0 until digits.length)
+            if (digits[i] != digits[lastIndex - i])
+                return false
+
+        return true
     }
 
 }
